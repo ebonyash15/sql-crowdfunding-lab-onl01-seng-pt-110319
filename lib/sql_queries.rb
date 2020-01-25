2e7106f396111d90ffe0fd8ea155534b846a43c4
Ebony Sharrock <ebonysharrock@gmail.com>
2020-01-25 11:11:25 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
   From pledges
   Inner Join users
   On Users.id = Pledges.user_id
-  GRoup by Pledges.user_id, Users.name"
+  GRoup by Users.name, Pledges.user_id"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
