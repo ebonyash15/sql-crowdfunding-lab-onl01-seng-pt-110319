de082cdf6906c2becb5b2065b7dc1dc475c63839
Ebony Sharrock <ebonysharrock@gmail.com>
2020-01-25 11:00:23 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,7 +15,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-  "Select Users.name, SUM(Pledges.amount)
+  "Select Users.name, Pledges.amount
   From Users
   Inner Join pledges
   On Users.id = Pledges.user_id
