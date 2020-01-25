53fb55ffbfe0f2658388cf9c2b38ff7c8ee5ca40
Ebony Sharrock <ebonysharrock@gmail.com>
2020-01-25 11:18:12 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,7 +23,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "Select projects.title, Sum(DISTINCT Pledges.amount)-Projects.funidng_goal
+  "Select projects.title, Sum(DISTINCT Pledges.amount)-Projects.funding_goal
   From projects
   WHERE pledges.project_id = projects.id GROUP BY Projects.title
 HAVING projects.id = pledges.project_id ORDER BY SUM(amount),
