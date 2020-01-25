8192881eefe4e97ca8fedae2311bc2b764a4b6cb
Ebony Sharrock <ebonysharrock@gmail.com>
2020-01-25 11:23:04 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -27,7 +27,8 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
   From projects
   Inner Join pledges
   On Projects.id = Pledges.project_id
-  Group By Projects.title, amount"
+  Group By Projects.title
+  Order by amount"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
