c58b03c145261fc53e010179876c1476c4cae9e9
Ebony Sharrock <ebonysharrock@gmail.com>
2020-01-25 11:24:22 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -27,6 +27,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
   From projects
   Inner Join pledges
   On Projects.id = Pledges.project_id
+  Where amount >=0
   Group By Projects.title
   Order by amount"
 end
