e81fd8219cd32e5687e52e51f3f00e9d4f11a1fa
Ebony Sharrock <ebonysharrock@gmail.com>
2020-01-25 11:17:51 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -25,13 +25,13 @@ end
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
   "Select projects.title, Sum(DISTINCT Pledges.amount)-Projects.funidng_goal
   From projects
-# Inner Join pledges
-#  On Projects.id = Pledges.project_id
-#  Group By Projects.title
   WHERE pledges.project_id = projects.id GROUP BY Projects.title
 HAVING projects.id = pledges.project_id ORDER BY SUM(amount),
 Projects.funding_goal > -1"
 end
+# Inner Join pledges
+#  On Projects.id = Pledges.project_id
+#  Group By Projects.title
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
   "Write your SQL query Here"
